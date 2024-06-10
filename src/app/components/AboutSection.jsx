bd9cbc29637b5d10c6999b506697b97e6166edac
Erick Godinez <forcet14@gmail.com>
2024-06-09 21:19:43 -0700

    commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-
 const AboutSection = () => {
   return (
     <section className="text-white font-body">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16"></div>
       <div>
-        <h2>
-About Me
-        </h2>
-        <p>
-        I'm a Software Engineer who loves to learn new things everyday to always keep my
-        brain healthy. Future M.S.E graduate in Software Engineer at Arizona State University in 2025.
+        <h2 className="text-6xl font-extrabold">About Me</h2>
+        <p className="text-5xl">
+          I'm a Software Developer who loves to learn new things everyday to
+          always keep my brain healthy.
+          <br></br>
+          <br></br>
+          Future M.S.E Software Engineer graduate at Arizona State University
+          in 2025.
         </p>
       </div>
     </section>
