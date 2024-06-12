ae076bd1d0d23c25a7bea498315aca6143d9b21c
Erick Godinez <forcet14@gmail.com>
2024-06-11 22:14:13 -0700

    about section update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,36 +1,103 @@
 "use client";
-import React, {useTransition,useState} from "react";
+import React, { useTransition, useState } from "react";
 import TabButton from "./TabButton";
 
-const AboutSection = () => {
-  const [tab,setTab]= useState("skills");
-  const [startTransition, isPending] = useTransition ();
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>JavaScript</li>
+        <li>CSS</li>
+        <li>TypeScript</li>
+        <li>Angular</li>
+        <li>React</li>
+        <li>Ionic</li>
+        <li>Drupal</li>
+        <li>PHP</li>
+        <li>Bootstrap</li>
+        <li>Tailwind</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>M.S.E Software Engineering at Arizona State University (2025)</li>
+        <li>B.S. Computer Engineering at Technological Institute of Tepic</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2 justify-items-center text-center">
+        <li>Google UX Design</li>
+      </ul>
+    ),
+  },
+];
 
-const handleTabChange = (id) => {
-  startTransition(() =>{
-    setTab(id);
-  })
-}
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
   return (
-    <section className="text-white font-body">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16"></div>
-      <div>
-        <h2 className=" text-center lg:text-6xl sm:text:3xl font-extrabold mb-8">About Me</h2>
+    <section className="text-white font-body" id="about">
+      <div className="md:grid md:grid-cols-1 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="mt-4 md:mt-0 text-center flex flex-col h-full">
+        <h2 className=" text-center text-base lg:text-6xl sm:text:3xl font-extrabold mb-8">
+          About Me
+        </h2>
         <p className="text-center text-5xl">
           I'm a Software Developer who loves to learn new things everyday to
-          always keep my brain healthy. I also love outer space, hence the name Supernova.
+          always keep my brain healthy. I also love outer space, hence the name
+          Supernova.
           <br></br>
           <br></br>
-          Future M.S.E Software Engineer graduate at Arizona State University
-          in 2025.
+          Future M.S.E Software Engineer graduate at Arizona State University in
+          2025.
         </p>
-        <div className="flex flex-row-8 mt-8">
-          <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}> Skills </TabButton>
-          <span className="mr-3 font-semibold hover:text-white text-[#DDDDDD] border-b border-[#F05454]">Skills</span>
-          <span>Education</span>
-          <span>Certifications</span>
+  
+        <div className="flex flex-row mt-8 text-center justify-start">
+          <TabButton
+            selectTab={() => handleTabChange("skills")}
+            active={tab === "skills"}
+          >
+            {" "}
+            Skills{" "}
+          </TabButton>
+          <TabButton
+            selectTab={() => handleTabChange("education")}
+            active={tab === "education"}
+          >
+            {" "}
+            Education{" "}
+          </TabButton>
+          <TabButton
+            selectTab={() => handleTabChange("certifications")}
+            active={tab === "certifications"}
+          >
+            {" "}
+            Certifications{" "}
+          </TabButton>
         </div>
+        
+        <div className="mt-8 text-center font-extralight">
+          {TAB_DATA.find((t) => t.id === tab).content}
+        </div>
+      </div>
       </div>
     </section>
   );
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const TabButton = ({active, selectTab, children}) => {
 const buttonClasses = active 
-? 'text-white border-b border-white :text-[#DDDDDD]'
-: 'text-red-500';
+? 'text-white border-b border-white :text-[#DDDDDD]': 'text-black';
 
   return (
     <button onClick={selectTab}>
